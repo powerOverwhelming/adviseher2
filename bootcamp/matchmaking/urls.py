8e6b014da96120dc821613f34de3cee065b8fb59
Matt <matt@1026labs.com>
2016-11-06 08:45:22 -0800

    Trying to get carousel to work.

--- bootcamp/matchmaking/urls.py
+++ bootcamp/matchmaking/urls.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from bootcamp.matchmaking import views
 
 urlpatterns = [
-    url(r'^$', views.matches, name='matchmaking'),
+    url(r'^match$', views.matches, name='matchmaking'),
 ]
--- bootcamp/matchmaking/views.py
+++ bootcamp/matchmaking/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 # Create your views here.
 
 def matches(request):
-    #matches = page_user = get_object_or_404(User)
-    #return render(request, 'matchmaking/carousel.html', {'matches': matches})
-    return 200
+    matches = page_user = User.objects.all()
+    return render(request, 'matchmaking/carousel.html', {'matches': matches})
+    #return render(request, 'matchmaking/carousel.html')
